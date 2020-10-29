45014e034feaeddf9d64009a7d9f77c378db1798
WilsonReese <reese@chicagobooth.edu>
2020-10-29 23:17:24 +0000

    Director Details Pages seem to work (no filmography yet)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
-  get("/directors/:id", { :controller => "directors", :action => "each_director" })
+  get("/directors/:dir_id", { :controller => "directors", :action => "each_director" })
 end

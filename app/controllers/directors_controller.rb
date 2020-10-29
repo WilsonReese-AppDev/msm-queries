b4456647e9fe5eba021a7f9d546e242e87edc59d
WilsonReese <reese@chicagobooth.edu>
2020-10-29 22:54:47 +0000

    RCAV for director details seems to work

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,4 +4,11 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/index.html.erb" })
   end
+
+  def each_director
+    list_of_directors = Director.all
+    @director_id = list_of_directors.at(params.fetch("id").to_i - 1).id
+
+    render({ :template => "director_templates/director_details.html.erb"})
+  end
 end

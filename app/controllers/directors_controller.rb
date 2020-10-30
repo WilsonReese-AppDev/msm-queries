2b9b357d28fc02bb7c38b99ed9e760fbcc10f4ec
WilsonReese <reese@chicagobooth.edu>
2020-10-30 23:06:18 +0000

    Added eldest and fixed director details

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,20 +5,29 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/index.html.erb" })
   end
 
-  def each_director
-    @list_of_directors = Director.all
-    @director_id = @list_of_directors.at(params.fetch("dir_id").to_i - 1).id
-    @director_name = @list_of_directors.at(params.fetch("dir_id").to_i - 1).name
-    @director_bio = @list_of_directors.at(params.fetch("dir_id").to_i - 1).bio
-    @director_image = @list_of_directors.at(params.fetch("dir_id").to_i - 1).image
-    @director_dob = @list_of_directors.at(params.fetch("dir_id").to_i - 1).dob
-    
-    require "date"
-    director_creation_date = @list_of_directors.at(params.fetch("dir_id").to_i - 1).created_at
-    director_updated_at = @list_of_directors.at(params.fetch("dir_id").to_i - 1).updated_at
+  def wisest
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).first
 
-    @age_of_record = Date.today.year - director_creation_date.year
-    @last_update = Date.today.year - director_updated_at.year
+    render({ :template => "director_templates/eldest.html.erb"})
+  end
+
+  def each_director
+  #   @list_of_directors = Director.all
+  #   @director_id = @list_of_directors.at(params.fetch("dir_id").to_i - 1).id
+  #   @director_name = @list_of_directors.at(params.fetch("dir_id").to_i - 1).name
+  #   @director_bio = @list_of_directors.at(params.fetch("dir_id").to_i - 1).bio
+  #   @director_image = @list_of_directors.at(params.fetch("dir_id").to_i - 1).image
+  #   @director_dob = @list_of_directors.at(params.fetch("dir_id").to_i - 1).dob
+    
+  #   require "date"
+  #   director_creation_date = @list_of_directors.at(params.fetch("dir_id").to_i - 1).created_at
+  #   director_updated_at = @list_of_directors.at(params.fetch("dir_id").to_i - 1).updated_at
+
+  #   @age_of_record = Date.today.year - director_creation_date.year
+  #   @last_update = Date.today.year - director_updated_at.year
+    the_id = params.fetch("dir_id")
+    @the_director = Director.where({ :id => the_id }).first
+    
 
     render({ :template => "director_templates/director_details.html.erb"})
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/eldest", { :controller => "directors", :action => "wisest"})
   get("/directors/:dir_id", { :controller => "directors", :action => "each_director" })
 end

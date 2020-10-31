7b477e9a5ef9f48e74ceacb6c77ca74eb8757b3e
WilsonReese <reese@chicagobooth.edu>
2020-10-31 16:40:07 +0000

    Youngest director (no Director Link)

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -11,6 +11,12 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/eldest.html.erb"})
   end
 
+  def littlest
+    @youngest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).last
+
+    render({ :template => "director_templates/youngest.html.erb"})
+  end
+
   def each_director
   #   @list_of_directors = Director.all
   #   @director_id = @list_of_directors.at(params.fetch("dir_id").to_i - 1).id
@@ -28,10 +34,7 @@ class DirectorsController < ApplicationController
     the_id = params.fetch("dir_id")
     @the_director = Director.where({ :id => the_id }).first
     @filmography = Movie.where({ :director_id => the_id })
-
-
     
-
     render({ :template => "director_templates/director_details.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/eldest", { :controller => "directors", :action => "wisest"})
+  get("/directors/youngest", { :controller => "directors", :action => "littlest"})
   get("/directors/:dir_id", { :controller => "directors", :action => "each_director" })
 end
